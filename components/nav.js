6e2f4ad8f7b1dc9f6c9b544a4346064807dcf008
João Lucas <jlcarruda3@gmail.com>
2020-10-11 14:37:10 -0300

    feat: added initial configurations and style for the first section of landing page

--- components/nav.js
+++ components/nav.js
@@ -1,23 +1,24 @@
 import Link from 'next/link'
+import navStyle from '../styles/nav.module.css'
 
 const links = [
-  { href: 'https://github.com/vercel/next.js', label: 'GitHub' },
-  { href: 'https://nextjs.org/docs', label: 'Docs' },
+  { href: 'https://github.com/vercel/next.js', label: 'Services' },
+  { href: 'https://nextjs.org/docs', label: 'About' },
+  { href: 'https://blink-ai.blogspot.com/', label: 'Blog' },
+  { href: 'https://blink-ai.blogspot.com/', label: 'Contact' }
 ]
 
 export default function Nav() {
   return (
-    <nav>
-      <ul className="flex justify-between items-center p-8">
-        <li>
-          <Link href="/">
-            <a className="text-blue-500 no-underline">Home</a>
-          </Link>
-        </li>
-        <ul className="flex justify-between items-center space-x-4">
+    <nav className={navStyle.nav}>
+      <Link href="/">
+        <a className="text-3xl text-gray-900 no-underline">Blink</a>
+      </Link>
+      <ul className="flex items-center">
+        <ul className="flex items-center space-x-12">
           {links.map(({ href, label }) => (
             <li key={`${href}${label}`}>
-              <a href={href} className="btn-blue no-underline">
+              <a href={href} className="no-underline">
                 {label}
               </a>
             </li>
